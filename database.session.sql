63ed37022548170c35fd05ce774d6ca41a7d8331
Matthew Machurek <mrjm2005@yahoo.com>
2021-02-26 10:22:45 -0600

    number 1

---
+++ database.session.sql
@@ -0,0 +1,9 @@
+Question 1
+SELECT a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id
+FROM animal a
+WHERE a.id = 3
---
+++ non_usa_customers.sql.session.sql
@@ -0,0 +1,6 @@
+-- Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+SELECT FirstName || ' ' || LastName AS EmployeeName,
+    CustomerId,
+    Country
+FROM Customer
+Where Country != 'USA'
